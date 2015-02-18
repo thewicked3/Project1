4c2394876db1d4d04036625002811dc0f4b598ba
thewicked3 <boat_yaguzaza@hotmail.com>
2015-02-18 10:16:01 +0700

    project1.2..212312
    lol

---
+++ app/src/main/java/com/example/silverwindz/project1/ExerciseCalculateActivity.java
@@ -0,0 +1,7 @@
+package com.example.silverwindz.project1;
+
+/**
+ * Created by SIlverwindz on 2/16/2015.
+ */
+public class ExerciseCalculateActivity {
+}
---
+++ app/src/main/java/com/example/silverwindz/project1/ExerciseListActivity.java
@@ -0,0 +1,9 @@
+package com.example.silverwindz.project1;
+
+/**
+ * Created by SIlverwindz on 2/16/2015.
+ */
+public class ExerciseListActivity {
+
+}
+
