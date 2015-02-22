a58676356bbe63c3db569ebc4a045c75def16f0d
silverwindz <silver9windz@gmail.com>
2015-02-22 18:12:38 +0700

    hell low

--- app/src/main/java/com/example/silverwindz/project1/ExerciseCalculateActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.silverwindz.project1;
-
-/**
- * Created by SIlverwindz on 2/16/2015.
- */
-public class ExerciseCalculateActivity {
-    int ffff;
-}
--- app/src/main/java/com/example/silverwindz/project1/ExerciseListActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.silverwindz.project1;
-
-/**
- * Created by SIlverwindz on 2/16/2015.
- */
-public class ExerciseListActivity {
-
-}
-
